54819b80a5e1d4c113bcaf4f902e30bc849d4375
BrkYld <burak_yildirim_official@hotmail.com>
2018-10-30 20:26:11 +0200

    Developed with Visual Studio IDE

---
+++ C#/C# PROJE/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.roketTimer = new System.Windows.Forms.Timer(this.components);
+            this.ucakTimer = new System.Windows.Forms.Timer(this.components);
+            this.kontrolTİmer = new System.Windows.Forms.Timer(this.components);
+            this.eklemeTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // roketTimer
+            // 
+            this.roketTimer.Interval = 1;
+            this.roketTimer.Tick += new System.EventHandler(this.roketTimer_Tick);
+            // 
+            // ucakTimer
+            // 
+            this.ucakTimer.Interval = 1;
+            this.ucakTimer.Tick += new System.EventHandler(this.ucakTimer_Tick);
+            // 
+            // kontrolTİmer
+            // 
+            this.kontrolTİmer.Interval = 1;
+            this.kontrolTİmer.Tick += new System.EventHandler(this.kontrolTİmer_Tick);
+            // 
+            // eklemeTimer
+            // 
+            this.eklemeTimer.Interval = 500;
+            this.eklemeTimer.Tick += new System.EventHandler(this.eklemeTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(972, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(1031, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "   0000";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.No;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(432, 206);
+            this.label3.MaximumSize = new System.Drawing.Size(500, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 38);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "OYUN BİTTİ";
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.BlanchedAlmond;
+            this.label4.Location = new System.Drawing.Point(278, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(524, 69);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "OYUNU BASLATMAK ICIN ENTER TUSUNA BASINIZ.\r\nUCAKSAVARI A/D TUSLARI ILE HAREKET ET" +
+    "TIRINIZ.\r\nATES ETMEK ICIN SPACE TUSUNA BASINIZ.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1182, 553);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Crimson;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer roketTimer;
+        private System.Windows.Forms.Timer ucakTimer;
+        private System.Windows.Forms.Timer kontrolTİmer;
+        private System.Windows.Forms.Timer eklemeTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ C#/C# PROJE/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int visibuSayi = 1; // Görünür ucak sayisini tutacak.
+        int uSayi, rSayi = 1;
+        int score = 0;
+        int hizDegiskeni = 2;
+        string labeltext;
+        
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            SoundPlayer arrow = new SoundPlayer();
+            arrow.SoundLocation = @"C:\Users\Yıldırım\Documents\Visual Studio 2015\Projects\WindowsFormsApplication1\WindowsFormsApplication1\arrow4.wav";
+            if (e.KeyCode == Keys.Enter && uSayi == 0)
+            {
+                label4.Visible = false;
+                ucakTimer.Enabled = true;
+                roketTimer.Enabled = true;
+                kontrolTİmer.Enabled = true;
+                eklemeTimer.Enabled = true;
+            }
+            else if (e.KeyCode == Keys.Space && roketTimer.Enabled == true)
+            {
+                roketEkle();
+                //arrow.Play();
+                
+            }
+            else if (e.KeyCode == Keys.A && roketTimer.Enabled == true)
+            {
+                ucakSavarSol();
+            }
+            else if (e.KeyCode == Keys.D && roketTimer.Enabled == true)
+            {
+                ucakSavarSag();
+            }
+
+        }
+
+        private void ucakSavarEkle()
+        {
+            PictureBox a = new PictureBox();
+            a.Name = "pcBoxCannon";
+            a.Width = 33;
+            a.Height = 34;
+            a.Location = new Point(425, 415);
+            a.Image = Properties.Resources.ucaksavar1;
+            this.Controls.Add(a);
+        }
+        void ucakSavarSol()
+        {
+
+            foreach (Control control in Controls)
+            {
+                if (control.GetType() == typeof(PictureBox))
+                {
+
+                    if (control.Name.Contains("pcBoxCannon"))
+                    {
+                        int index = control.Location.X - control.Width;
+                        if (index < 0)
+                        {
+                            index = control.Location.X;
+                        }
+                        control.Location = new Point(index, control.Location.Y);
+                    }
+
+                }
+
+            }
+
+        }
+
+        void ucakSavarSag()
+        {
+
+            foreach (Control control in Controls)
+            {
+                if (control.GetType() == typeof(PictureBox))
+                {
+
+                    if (control.Name.Contains("pcBoxCannon"))
+                    {
+                        int index = control.Location.X + control.Width;
+                        if (index > 875)
+                        {
+                            index = control.Location.X;
+                        }
+                        control.Location = new Point(index, control.Location.Y);
+                    }
+
+                }
+
+            }
+
+
+
+
+        }
+        private void roketEkle()
+        {
+
+            PictureBox b = new PictureBox();
+
+            b.Name = "pcBoxRocket" + rSayi;
+
+            b.Width = 15;
+            b.Height = 16;
+            b.Image = Properties.Resources.mermi;
+            this.Controls.Add(b);
+            rSayi++;
+            foreach (Control control in Controls)
+            {
+                if (control.GetType() == typeof(PictureBox))
+                {
+
+                    if (control.Name.Contains("pcBoxCannon"))
+                    {
+                        b.Location = new Point(control.Location.X + 10, control.Location.Y - 1);
+                    }
+
+                }
+
+            }
+
+        }
+        private void ucakEkle()
+        {
+            PictureBox c = new PictureBox();
+
+            c.Name = "pcBoxUcak" + uSayi;
+            c.Location = new Point(RastgeleSayi.SayiUret(5, this.Width - 100), 5);
+            c.Width = 71;
+            c.Height = 73;
+            c.Image = Properties.Resources.ucak1;
+            this.Controls.Add(c);
+            uSayi++;
+            visibuSayi++;
+        }
+
+        private void roketHareket(Control rH)
+        {
+            for (var a = 0; a < 5; a++)
+            {
+                rH.Location = new Point(rH.Location.X, rH.Location.Y - 1);
+                carpismaKontrol();
+            }
+            if (rH.Location.Y < 0)
+            {
+                this.Controls.Remove(rH);
+            }
+
+        }
+        private void ucakHareket(Control uH)
+        {
+
+            SoundPlayer defeat = new SoundPlayer();
+            defeat.SoundLocation = @"C:\Users\Yıldırım\Documents\Visual Studio 2015\Projects\WindowsFormsApplication1\WindowsFormsApplication1\defeat.wav";
+
+            uH.Location = new Point(uH.Location.X, uH.Location.Y + 1);
+            carpismaKontrol();
+
+            if (uH.Location.Y > 413)
+            {
+                label3.Visible = true;
+                this.Controls.Remove(uH);
+                eklemeTimer.Enabled = false;
+                ucakTimer.Enabled = false;
+                roketTimer.Enabled = false;
+                //defeat.Play();
+               
+            }
+
+
+
+
+
+
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;//Boyutu sabitlemeye yarar.
+            ucakSavarEkle();
+        }
+
+        private void ucakTimer_Tick(object sender, EventArgs e)
+        {
+
+
+            foreach (Control control in Controls)
+            {
+                if (control.GetType() == typeof(PictureBox))
+                {
+
+                    if (control.Name.Contains("pcBoxUcak"))
+                    {
+                        ucakHareket(control);
+                    }
+
+
+
+
+
+
+                }
+
+            }
+        }
+
+        private void kontrolTİmer_Tick(object sender, EventArgs e)
+        {
+            carpismaKontrol();
+
+
+
+        }
+
+        private void carpismaKontrol()
+        {
+            SoundPlayer patlama = new SoundPlayer();
+            patlama.SoundLocation = @"C:\Users\Yıldırım\Documents\Visual Studio 2015\Projects\WindowsFormsApplication1\WindowsFormsApplication1\cannon4_new.wav";
+            foreach (Control roket in Controls)
+            {
+                if (roket.GetType() == typeof(PictureBox))
+                {
+                    if (roket.Name.Contains("pcBoxRocket") && roket.Visible)
+                    {
+                        foreach (Control ucak in Controls)
+                        {
+                            if (ucak.GetType() == typeof(PictureBox))
+                            {
+                                if (ucak.Name.Contains("pcBoxUcak") && ucak.Visible)
+                                {
+                                    if (roket.Location.X >= ucak.Location.X && roket.Location.X <= ucak.Location.X + ucak.Width
+                                    && (ucak.Location.Y + ucak.Height == roket.Location.Y + roket.Height))
+                                    {
+                                        this.Controls.Remove(roket);
+                                        this.Controls.Remove(ucak);
+                                        visibuSayi--;
+                                        score = score + 100;
+                                        labeltext = Convert.ToString(score);
+                                        label2.Text = labeltext;
+                                        //patlama.Play();
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void eklemeTimer_Tick(object sender, EventArgs e)//Ucak sayisi artirma.
+        {
+            if (visibuSayi <= hizDegiskeni)
+            {
+                ucakEkle();
+            }
+
+            if (score % 1000 == 0 && score != 0)
+            {
+                hizDegiskeni++;
+                score = score + 100;
+            }
+
+
+        }
+
+        private void roketTimer_Tick(object sender, EventArgs e)
+        {
+            foreach (Control control in Controls)
+            {
+                if (control.GetType() == typeof(PictureBox))
+                {
+
+                    if (control.Name.Contains("pcBoxRocket"))
+                    {
+                        roketHareket(control);
+
+                    }
+                }
+
+            }
+        }
+
+      
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Enabled = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            label2.Enabled = true;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            label3.Visible = false;
+        }
+
+       
+    }
+    class RastgeleSayi
+    {
+        public static int SayiUret(int min, int max)
+        {
+            if (rastgele == null)
+                rastgele = new Random();
+
+            return rastgele.Next(min, max);
+        }
+
+        private static Random rastgele;
+    }
+}
